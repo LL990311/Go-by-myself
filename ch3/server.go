cd91bf10a163d4be319a3769809c62c1bfb78485
LukeLong <lzx19990311@gmail.com>
2022-11-11 10:43:24 +0800

    upload: surface

--- ch3/server.go
+++ ch3/server.go
@@ -1,11 +1,19 @@
-package surface
+package main
 
-import "net/http"
+import (
+	"Go-by-myself/ch3/surface"
+	"log"
+	"net/http"
+)
 
 func main() {
 
+	//e3.4 upload surface on the localhost:8080 server
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Content-Type", "image/svg+xml")
-		surface()
+		surface.Surface(writer)
 	})
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+
 }
--- ch3/surface/surface.go
+++ ch3/surface/surface.go
@@ -2,6 +2,7 @@ package surface
 
 import (
 	"fmt"
+	"io"
 	"math"
 	"os"
 )
@@ -17,9 +18,9 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
-func Surface() {
+func Surface(w io.Writer) {
 	//e3.3 改变峰顶以及谷底颜色
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: #ff0000; fill:#0000ff; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 
@@ -29,11 +30,11 @@ func Surface() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' />\n",
+			fmt.Fprintf(w, "<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' />\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintf(w, "</svg>")
 }
 
 func corner(i, j int) (float64, float64) {
