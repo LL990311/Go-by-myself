7fdebc13c8f462e81f769e7698e3016e2009f7a7
LukeLong <lzx19990311@gmail.com>
2022-11-23 11:57:40 +0800

    update excercise

--- ch4/reverseUtf8.go
+++ ch4/reverseUtf8.go
@@ -1,10 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 func main() {
 	b := []byte("哈哈哈 嘿嘿嘿 嚯嚯嚯")
-	reverseUtf8(b)
+	revUTF8(b)
+	fmt.Printf("%s\n", b)
 }
 
-func reverseUtf8(arr []byte) {
+func revUTF8(b []byte) {
+	for i := 0; i < len(b); {
+		fmt.Println(b)
+		_, size := utf8.DecodeRune(b[i:])
+		rev(b[i : i+size])
+		fmt.Println(b)
+		fmt.Println("----------------------------------")
+		i += size
+	}
+	rev(b)
+	fmt.Println(b)
+}
 
+func rev(b []byte) {
+	last := len(b) - 1
+	for i := 0; i < len(b)/2; i++ {
+		b[i], b[last-i] = b[last-i], b[i]
+	}
 }
