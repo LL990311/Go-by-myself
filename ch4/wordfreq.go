e9df72d2e5b12b0115011f78c58203295b7a1421
LukeLong <lzx19990311@gmail.com>
2022-11-23 17:42:43 +0800

    update excercise

---
+++ ch4/wordfreq.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	freq := make(map[string]int)
+	io := bufio.NewScanner(os.Stdin)
+	io.Split(bufio.ScanWords)
+	for io.Scan() {
+		freq[io.Text()]++
+	}
+	for k, v := range freq {
+		fmt.Printf("%s\t%d\n", k, v)
+	}
+}
