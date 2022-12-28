7fa5805d67bba5042123f24a1e7f708151dea018
LukeLong <lzx19990311@gmail.com>
2022-12-28 16:11:27 +0800

    update

--- ch5/findlinks1.go
+++ ch5/findlinks1.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	"golang.org/x/net/html"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -13,9 +13,19 @@ func main() {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
 		os.Exit(1)
 	}
+
 	for _, link := range visit(nil, doc) {
 		fmt.Println(link)
 	}
+
+	////ex 5.2
+	//freq := make(map[string]int, 0)
+	//for key, value := range elementCounts(freq, doc) {
+	//	fmt.Printf("%-8s:%d\n", key, value)
+	//}
+
+	////ex 5.3
+	//output(doc)
 }
 
 //// iteration
@@ -33,11 +43,18 @@ func main() {
 //	return links
 //}
 
-//recursion (ex5.1)
+//recursion (ex5.1) (ex5.4)
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	if n.Type == html.ElementNode && n.Data == "script" || n.Data == "style" || n.Data == "img" {
+		for _, a := range n.Attr {
+			if a.Key == "src" {
 				links = append(links, a.Val)
 			}
 		}
@@ -49,5 +66,34 @@ func visit(links []string, n *html.Node) []string {
 		links = visit(links, n.NextSibling)
 	}
 	return links
+}
 
+//ex5.2
+func elementCounts(freq map[string]int, n *html.Node) map[string]int {
+	if n.Type == html.ElementNode {
+		freq[n.Data]++
+	}
+	if n.FirstChild != nil {
+		elementCounts(freq, n.FirstChild)
+	}
+	if n.NextSibling != nil {
+		elementCounts(freq, n.NextSibling)
+	}
+	return freq
+}
+
+//ex 5.3
+func output(n *html.Node) {
+	if n.Type == html.TextNode {
+		s := strings.Join(strings.Fields(n.Data), " ")
+		if s != "" {
+			fmt.Println(s)
+		}
+	}
+	if n.FirstChild != nil {
+		output(n.FirstChild)
+	}
+	if n.NextSibling != nil {
+		output(n.NextSibling)
+	}
 }
--- ch5/outline.go
+++ ch5/outline.go
@@ -15,26 +15,12 @@ func main() {
 	outline(nil, doc)
 }
 
-//func outline(stack []string, n *html.Node) {
-//	if n.Type == html.ElementNode {
-//		stack = append(stack, n.Data)
-//		fmt.Println(stack)
-//	}
-//	for c := n.FirstChild; c != nil; c = c.NextSibling {
-//		outline(stack, c)
-//	}
-//}
-
-//ex5.2 calculate the counts of elements
-func outline(stack []string, n *html.Node) int64 {
-	var counts int64 = 0
+func outline(stack []string, n *html.Node) {
 	if n.Type == html.ElementNode {
 		stack = append(stack, n.Data)
-		fmt.Println(n.Type)
-		counts++
+		fmt.Println(stack)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		outline(stack, c)
 	}
-	return counts
 }
